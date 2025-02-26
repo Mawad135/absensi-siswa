14d5b53ccebe7ab19f9c68fc36de9b9d5f665d76
Mawad135 <memeddingin@gmail.com>
2025-02-25 23:12:45 -0800

    ADD dashboard-login

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/admin', function () {
+    return view('index');
+});
+
+Route::get('/halaman_login', function()
+{
+    return view('login',[
+        "menu"=>"login"
+    ]);
+})->name('login');
